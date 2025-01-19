87baa385652dcb1214dc898aa9c88cd08d5e09b3
Bonfra04 <bonfract@gmail.com>
2025-01-19 17:20:13 +0100

    utility methods for track

--- lib/logic/data/track.dart
+++ lib/logic/data/track.dart
@@ -13,5 +13,17 @@ class Playlist {
     final List<Track> tracks;
     final Uri cover;
 
+    int get length => this.tracks.length;
+
     const Playlist(this.name, this.tracks, this.cover);
+
+    Track operator [](int index) => this.tracks[index];
+
+    Playlist copyWith({String? name, List<Track>? tracks, Uri? cover}) {
+        return Playlist(
+            name ?? this.name,
+            tracks ?? this.tracks,
+            cover ?? this.cover
+        );
+    }
 }
