24ee84a657022d749fc8e4b4cf85231cb2687438
Bonfra04 <bonfract@gmail.com>
2025-01-19 17:20:34 +0100

    stream driven implementation of events for audio player

--- lib/logic/player/player.dart
+++ lib/logic/player/player.dart
@@ -1,25 +1,79 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:spacchiofy/logic/data/track.dart';
 
+final class ControllerStream {
+    final Stream<Playlist?> playlist;
+    final Stream<int> playlistIndex;
+
+    const ControllerStream({
+        required this.playlist,
+        required this.playlistIndex
+    });
+}
+
 final class StronzAudioPlayer {
+    static final StronzAudioPlayer instance = StronzAudioPlayer._();
     StronzAudioPlayer._();
 
-    static final AudioPlayer _player = AudioPlayer();
-    static final List<Track> _queue = [];
-
-    static void enqueue(Track track) {
-       StronzAudioPlayer._queue.add(track);
+    Playlist? _playlist;
+    Playlist? get playlist => this._playlist;
+    final StreamController<Playlist?> _playlistStreamController = StreamController<Playlist?>.broadcast();
+    @protected
+    set playlist(Playlist? playlist) {
+        if(this._playlist != playlist)
+            this._playlistStreamController.add(this._playlist = playlist);
     }
 
-    static Future<void> play() async {
-        await _player.play();
+    int _playlistIndex = 0;
+    int get playlistIndex => this._playlistIndex;
+    final StreamController<int> _playlistIndexStreamController = StreamController<int>.broadcast();
+    @protected
+    set playlistIndex(int playlistIndex) {
+        if(this._playlistIndex != playlistIndex)
+            this._playlistIndexStreamController.add(this._playlistIndex = playlistIndex);
     }
 
-    static Future<void> pause() async {
-        await _player.pause();
+    ControllerStream get stream => ControllerStream(
+        playlist: this._playlistStreamController.stream,
+        playlistIndex: this._playlistIndexStreamController.stream
+    );
+
+    void load(Playlist playlist) {
+        this._playlistIndex = 0;
+        this.playlist = playlist;
     }
 
-    static Future<void> stop() async {
-        await _player.stop();
+    void unload() => this.playlist = null;
+
+    void enqueue(Track track) {
+        this.playlist = this._playlist!.copyWith(
+            tracks: [...this._playlist!.tracks, track]
+        );
     }
+
+    void prev() {
+        this.playlistIndex = (this._playlistIndex - 1) % this._playlist!.length;
+    }
+
+    void next() {
+        this.playlistIndex = (this._playlistIndex + 1) % this._playlist!.length;
+    }
+
+    void pause() {
+    }
+
+    // Future<void> play() async {
+    //     await _player.play();
+    // }
+
+    // Future<void> pause() async {
+    //     await _player.pause();
+    // }
+
+    // Future<void> stop() async {
+    //     await _player.stop();
+    // }
 }
